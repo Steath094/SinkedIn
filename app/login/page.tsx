d0dcf9ac8989fc7fce3820148633538a19e28acd
Sam <armandawoodkhan@gmail.com>
2025-06-10 03:26:13 +0530

    feat: create initial Login page UI

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,19 +1,22 @@
 "use client"
 import { signIn } from "next-auth/react"
-import { useState } from "react"
+import { useRef, useState } from "react"
 import { useRouter } from "next/navigation"
+import Link from "next/link"
+import Image from "next/image"
 
 export default function LoginPage() {
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
+
+   const emailRef = useRef<HTMLInputElement>(null)
+    const passwordRef = useRef<HTMLInputElement>(null)
   const router = useRouter()
 
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault()
     const res = await signIn("credentials", {
       redirect: false,
-      username: email,
-      password,
+      username: emailRef.current?.value,
+      password: passwordRef.current?.value
     })
 
     if (res?.ok) router.push("/")
@@ -21,10 +24,20 @@ export default function LoginPage() {
   }
 
   return (
-    <form onSubmit={handleLogin} className="p-4 space-y-4">
-      <input type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="Email" />
-      <input type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Password" />
-      <button type="submit">Login</button>
-    </form>
+     <section className="w-full h-screen flex justify-center items-center flex-col">
+      <div className="absolute top-10 left-50 "><Image src={'/logo.jpg'} alt="logo" width={60} height={60}></Image><p className="text-5xl"></p></div>
+      <div className="w-1/4 shadow-lg rounded-md p-4">
+        <form onSubmit={handleLogin} className="p-4 flex flex-col">
+          <label htmlFor="email" className="text-[14px] font-semibold  mt-4 mb-1">Enter Your Email</label>
+          <input type="email" ref={emailRef} className="px-4 py-2  border border-gray-500 rounded-md" />
+          <label htmlFor="email" className="text-[14px] font-semibold  mt-4 mb-1">Enter Your Password</label>
+          <input type="password" ref={passwordRef} className="px-4 py-2  border border-gray-500 rounded-md" />
+
+          <p className="text-[12px] font-light text-center mt-6">By clicking Continue to join or sign in, you agree to SinkedIn's <span className="text-blue-600 font-medium hover:underline cursor-pointer">User Agreement</span>,<span className="text-blue-600 font-medium hover:underline cursor-pointer"> Privacy Policy</span>, and <span className="text-blue-600 font-medium hover:underline cursor-pointer">Cookie Policy.</span></p>
+          <button type="submit" className="mt-6 border-2 py-3 px-4 w-full rounded-full text-white bg-[#0a66c2] hover:bg-[#004182] cursor-pointer">Sign in</button>
+        </form>
+      </div>
+      <p className="text-center p-6">New to SinkedIn? <span className="text-blue-600 font-medium hover:underline cursor-pointer"><Link href={'/signup'}>Join now </Link></span></p>
+    </section>
   )
 }
